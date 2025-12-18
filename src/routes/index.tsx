104d0c9ad052ecea9b65191c50baf61500557c27
NazarK0 <vanivskyi.dev@gmail.com>
2025-12-18 15:12:50 +0200

    fix router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,30 @@
 // src/routes/index.tsx
+import { useState, useEffect, lazy, Suspense } from 'react'; 
 import { createFileRoute } from '@tanstack/react-router'
+const AdminUI = lazy(() => import('../Admin')); 
+const UserUI = lazy(() => import('../User'));
+import HomePage from '../components/global/HomePage';
+import Loading from '../shared/Loading';
 
 export const Route = createFileRoute('/')({
   component: () => {
     const { auth } = Route.useRouteContext()
     
     if (auth.user?.role === 'admin') {
-      const AdminUI = lazy(() => import('../Admin')); 
-      return <AdminUI />
+      return (
+      	<Suspense fallback={<Loading />}> 
+          <AdminUI />
+      	</Suspense>
+      );
     } else if (auth.user?.role === 'user') {
 
-      const UserUI = lazy(() => import('../User')); 
-      return <UserUI />
+       
+      return (
+       	<Suspense fallback={<Loading />}> 
+           <UserUI />
+       	</Suspense>
+      );
     } else {
-    	import HomePage from './components/global/HomePage';
     	return <HomePage />
     }
   },
